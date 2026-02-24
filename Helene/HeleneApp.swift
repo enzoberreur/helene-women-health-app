7490401beaa5911dde32bf991eac547e387dda3b
Enzo Berreur <enzo.berreur@gmail.com>
2026-02-24 21:12:54 +0100

    Initial Commit

---
+++ Helene/HeleneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeleneApp.swift
+//  Helene
+//
+//  Created by Enzo Berreur on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HeleneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeleneTests/HeleneTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeleneTests.swift
+//  HeleneTests
+//
+//  Created by Enzo Berreur on 24/02/2026.
+//
+
+import Testing
+@testable import Helene
+
+struct HeleneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
